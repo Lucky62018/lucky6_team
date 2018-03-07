0b4bf9f204887eb9aa29e7a406e856cd3694fe6c
Jovi <gnitejovi@gmail.com>
2018-03-07 11:00:19 +0000

    finish materials and positionale camera

--- Backend/Camera.h
+++ Backend/Camera.h
@@ -10,15 +10,30 @@ class Camera {
     Vector3D horizontal;
     Vector3D vertical;
 
-    Camera() {
-      leftLowerCorner = Vector3D(-2, -1, -1);
-      horizontal = Vector3D(4, 0, 0);
-      vertical = Vector3D(0, 2, 0);
-      origin = Vector3D(0, 0, 0);
+    Camera(Vector3D from, Vector3D at, Vector3D vup, float vfov, float aspect) { // vfov is top to bottom in degrees
+      Vector3D u, v, w;
+      float theta = vfov * M_PI / 180;
+      float halfHeight = tan(theta/2);
+      float halfWidth = aspect * halfHeight;
+      origin = from;
+      w = GetUnitizedCopy(from - at);
+      u = GetUnitizedCopy(CrossProduct(vup, w));
+      v = CrossProduct(w, u);
+      leftLowerCorner = origin - halfWidth * u - halfHeight * v - w;
+      horizontal = 2 * halfWidth * u;
+      vertical = 2 * halfHeight * v;
     }
 
-    Ray getRay(float u, float v) {
-      return Ray(origin, leftLowerCorner + u * horizontal + v * vertical - origin);
+    // Camera() {
+    //   leftLowerCorner = Vector3D(-2, -1, -1);
+    //   horizontal = Vector3D(4, 0, 0);
+    //   vertical = Vector3D(0, 2, 0);
+    //   origin = Vector3D(0,0,0);
+    // }
+
+
+    Ray getRay(float a, float b) {
+      return Ray(origin, leftLowerCorner + a * horizontal + b * vertical - origin);
     }
 
 };
--- Backend/README.md
+++ Backend/README.md
@@ -1,1 +1,14 @@
 # Backend
+
+# Current Features:
+
+	1. Shapes:
+		* Sphere
+
+	2. Materials:
+		* Diffuse
+		* Metal
+		* Glass
+
+	3. Camera:
+		Camera can be set to any position.
